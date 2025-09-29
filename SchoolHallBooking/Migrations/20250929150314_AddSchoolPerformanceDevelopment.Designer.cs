cb6bf6e1a3418a24e85f320d98ef79e20616e148
alazhar98 <alazhar9814@gmail.com>
2025-09-29 19:03:43 +0400

    EF migration: AddSchoolPerformanceDevelopment (tables for visit analyses and student work)

--- SchoolHallBooking/Migrations/20250929150314_AddSchoolPerformanceDevelopment.Designer.cs
+++ SchoolHallBooking/Migrations/20250929150314_AddSchoolPerformanceDevelopment.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolHallBooking.Data;
 
@@ -10,9 +11,11 @@ using SchoolHallBooking.Data;
 namespace SchoolHallBooking.Migrations
 {
     [DbContext(typeof(BookingDbContext))]
-    partial class BookingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250929150314_AddSchoolPerformanceDevelopment")]
+    partial class AddSchoolPerformanceDevelopment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
